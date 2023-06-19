673a91d147f209510c4bdf0c02d73b235bd77974
rakshith299 <133012343+rakshith299@users.noreply.github.com>
2023-06-19 18:30:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,29 @@ const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
+let table = document.getElementById("table");
+
 const getSum = () => {
-//Add your code here
+    let allPrices = document.querySelectorAll("td[class = price]");
+    
+    
+    let sum = 0;
+
+    for(let i= 0; i < allPrices.length; i++){
+        sum = sum + parseInt(allPrices[i].innerHTML.trim());
+
+    }
+
+    let newRow = document.createElement("tr");
+    let cell1 = document.createElement("td");
+    cell1.textContent = "Total is :";
+    let cell2 = document.createElement("td");
+    cell2.textContent = sum;
+
+    newRow.appendChild(cell1);
+    newRow.appendChild(cell2);
+
+    table.appendChild(newRow);
   
 };
 
